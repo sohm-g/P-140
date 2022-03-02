ce795af9ce7f099481d68a6db8d90fb959c41df7
sohm-g <77761242+sohm-g@users.noreply.github.com>
2022-03-01 17:46:05 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var paddle2 = 10;
+var paddle1 = 10;
+var paddle1X = 10;
+var paddle1height = 110;
+var paddle2Y = 685;
+var paddle2height = 70;
+var score1 = 0;
+var score2 = 0;
+var paddle1Y;
+var playerscore = 0;
+var pcscore = 8;
+var ball = {
+  x:350/2,
+  y:480/2,
+  r:20,
+  dx:3,
+  dy:3
+}
+
+rightWistY = 0;
+rightWistX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
